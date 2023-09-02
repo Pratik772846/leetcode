9ffd8fa74fe0e7fc4c5919f4c8af59b3e40b0b55
Pratik <97464067+Pratik772846@users.noreply.github.com>
2023-09-03 00:11:45 +0530

    Time: 140 ms (69.46%), Space: 80 MB (60.05%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -4,11 +4,6 @@ public:
         int n = s.size();
         vector<int> dp(n+2,0);
         unordered_set<string> dictionarySet(dictionary.begin(),dictionary.end());
-        // int len =0;
-        // for(auto it:dictionary){
-        //     int a = it.size();
-        //     len = max(len,a);
-        // }
         for (int start = n - 1; start >= 0; start--) {
             dp[start] = dp[start + 1] + 1;
             for (int end = start; end < n; end++) {
